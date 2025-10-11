aafc36cd1015de46dacd493a154bd108a2c5485b
Ian Atkin <iwatkin@gmail.com>
2025-10-11 01:12:13 -0700

    uh-oh

---
+++ target/release/build/num-traits-ebf699424169e5d3/build_script_build-ebf699424169e5d3.d
@@ -0,0 +1,5 @@
+/Users/ian/Development/rust/astra/target/release/build/num-traits-ebf699424169e5d3/build_script_build-ebf699424169e5d3.d: /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
+
+/Users/ian/Development/rust/astra/target/release/build/num-traits-ebf699424169e5d3/build_script_build-ebf699424169e5d3: /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs
+
+/Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-traits-0.2.19/build.rs:
