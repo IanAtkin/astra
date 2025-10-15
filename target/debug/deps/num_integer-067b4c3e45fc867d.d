230160dc65c36c6569b5593214bbc65427330f88
Ian Atkin <ian@versal.com>
2025-10-14 19:19:42 -0700

    Delete target/debug/deps directory
    no

--- target/debug/deps/num_integer-067b4c3e45fc867d.d
+++
@@ -1,9 +0,0 @@
-/Users/ian/Development/rust/astra/target/debug/deps/num_integer-067b4c3e45fc867d.d: /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
-
-/Users/ian/Development/rust/astra/target/debug/deps/libnum_integer-067b4c3e45fc867d.rlib: /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
-
-/Users/ian/Development/rust/astra/target/debug/deps/libnum_integer-067b4c3e45fc867d.rmeta: /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs /Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs
-
-/Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/lib.rs:
-/Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/roots.rs:
-/Users/ian/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-integer-0.1.46/src/average.rs:
